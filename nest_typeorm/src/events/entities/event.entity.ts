ef9cd0871bd4751e003789bf95e09fd157b1782e
Tahmid Ali <tahmid.tee10@gmail.com>
2022-10-07 00:33:20 +0600

    data type

--- nest_typeorm/src/events/entities/event.entity.ts
+++ nest_typeorm/src/events/entities/event.entity.ts
@@ -10,7 +10,7 @@ export class Event {
   name: string;
 
   @Column({ type: 'datetime' })
-  createdAt: string;
+  createdAt: Date;
 
   @OneToMany(() => Workshop, (workshop) => workshop.eventId)
   workshops!: Workshop[];
